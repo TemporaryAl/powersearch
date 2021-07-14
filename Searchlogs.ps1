9e7c3f936aaa92cd9ca8c611bf569fc579276387
TemporaryAl <87444990+TemporaryAl@users.noreply.github.com>
2021-07-14 21:52:22 +0200

    Initial version

---
+++ Searchlogs.ps1
@@ -0,0 +1,3 @@
+﻿$Searchpattern = Read-Host -Prompt 'Input the pattern you want to search for in the logs'
+Get-ChildItem -Path "c:\users\username\desktop\srb*\*.txt" -Recurse -Force | sls $Searchpattern
+Read-Host -Prompt 'Press enter to close...'
